2e78d8d9db0b7333ac625df9a38e521fd81bc8b5
Сабина Кадырова <sabina@Sabinas-MBP.Dlink>
2020-02-23 18:00:07 +0200

    Separation of components. Stylezation.

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoItem from '../TodoItem/TodoItem';
+import './App.css';
+
+function App() {
+    return (
+        <div className="todo-list">
+            <TodoItem />
+        </div>
+    )
+}
+
+export default App;
