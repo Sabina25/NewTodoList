72a5817faa60fa59738269e9d804e25412fd0f64
Сабина Кадырова <sabina@Sabinas-MBP.Dlink>
2020-02-24 20:43:52 +0200

    work with props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import TodoItem from '../TodoItem/TodoItem';
+import todosData from '../../todosData';
 import './App.css';
 
 function App() {
+    
+    const todoItems = todosData.map(item => {
+        return <TodoItem key={item.id} item={item}/>
+    })
+
     return (
         <div className="todo-list">
-            <TodoItem />
+            {todoItems}
         </div>
     )
 }
