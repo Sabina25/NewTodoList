cdad58041a7080a86008f65b6308fd15c8d3cabd
Сабина Кадырова <sabina@Sabinas-MBP.Dlink>
2020-02-25 23:04:18 +0200

    event handling. Clicking on checkbox

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import TodoItem from '../TodoItem/TodoItem';
 import todosData from '../../todosData';
 import './App.css';
 
-function App() {
-    
-    const todoItems = todosData.map(item => {
-        return <TodoItem key={item.id} item={item}/>
-    })
+class App extends Component {
+    constructor () {
+        super ();
+        this.state = {
+            todos: todosData,
+        }
+        
+        this.handleClick=this.handleClick.bind(this)
+    }
 
-    return (
-        <div className="todo-list">
-            {todoItems}
-        </div>
-    )
+    handleClick(id){
+        this.setState (prevState => {
+            const utdatesTodos = prevState.todos.map(todo=>
+                {
+                    if (todo.id===id) {
+                        todo.completed = !todo.completed
+                    }
+
+                    return todo
+                })
+
+            return {
+                todos: utdatesTodos
+            }
+        })
+    }
+
+    render () {
+        
+        const todoItems = this.state.todos.map(item => {
+            return <TodoItem key={item.id} 
+                            item={item}  
+                            handleClick={this.handleClick}/>
+        })
+    
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )
+    }
 }
 
 export default App;
--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -4,7 +4,8 @@ import './TodoItem.css';
 function  TodoItem (props) {
     return (
         <div className="todo-item">
-            <input type="checkbox" checked={props.item.completed}/>
+            <input type="checkbox" checked={props.item.completed} 
+                    onClick={()=>props.handleClick(props.item.id)}/>
             <p>{props.item.text}</p>
         </div>
     );
